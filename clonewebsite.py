362ea738472f6790c2e7c4b98a8b557f92d224ec
Nathan <nathanh4903@gmail.com>
2023-02-21 23:41:39 -0500

    included extra files and added in banner removers
    -updated clonewebsite.py to clean ad banners
    -404.html
    -robots.txt

--- clonewebsite.py
+++ clonewebsite.py
@@ -1,38 +1,63 @@
 domain = "utmccf.wordpress.com"
 
-downloadAgain=True
+downloadAgain=False
 resultzip=False
 verbose=False
 testOnlyOneFile=False
 removeExtraFiles=False
 tagsToRemove=(
   #marketing bar
-  r"""<div id="marketingbar".*?marketing_bar"><\/a><\/div>""", 
+  r"""<div id="marketingbar".*?marketing_bar"><\/a><\/div>""",
   #remove the blank space above main menu e.g.   <a href="index.html" class="site-logo-link" rel="home" itemprop="url"></a>
   r"""<a href=.*?site-logo-link.*?>.*?</a>""", 
   #footer ad, e.g. <div class="site-info"><a href="https://wordpress.com/?ref=footer_blog" rel="nofollow">Blog at WordPress.com.</a></div>
   r"""<div.class=.site-info.>[\s\S]*?log at WordPress.com[\s\S]*?</div>""",
-  
   r"""<script id="wpcom_remote_login_js">[\s\S]*?<\/script>""", #wp login  
-  r"""<script\b[^>]*>[^>]*?actionbar[\s\S]*?<\/script\b[^>]*>""",
+  r"""<script\b[^>]*>[^>]*?actionbar[\s\S]*?<\/script\b[^>]*>""", #actionbar
+  r"""<!-- CCPA \[start\] -->[\s\S]*?<!-- CCPA \[end\] -->""", #<!-- CCPA [start] -->blabla<!-- CCPA [end] -->
+  r"""<div.*?class="widget widget_eu_cookie_law_widget".*?>[\s\S]*?</div>[\s]*?</div>""", #cookie banner
 
-  #r"""<script[\s\S]*?>[\s\S]*?<\/script>""", #all <script> tags
   ############below are optional optimization####################
   r"""<link.*rel=.pingback..*?>""", #pingbacks like <link rel="pingback" href="https://tendertastytester.wordpress.com/xmlrpc.php">
   r"""<link.*rel='dns-prefetch'.*/>""", #dns prefetches
-)  
+)
 
+scriptKeywords=(
+  "insertInlineAds",
+  "google_analytics",
+  "google-analytics",
+  "Advertisements",
+  "advertising",
+  "sascdn.com",
+  "smartadserver",
+  "outbrain",
+  "gdpr"
+)
 
 import os, re,time
 startTime=time.time()
 resultPath=f'clean_{domain}'
 doNotShowShellResult=">run.log 2>&1"#">/dev/null 2>&1 "
+
+def contains(text,keywordList):
+  for keyword in keywordList:
+    if keyword in text:
+      return True
+  return False
+
+def findUnwantedScripts(text): #look for all <script> tags
+  toBeRemoved=[]
+  for scriptTag in (re.findall(r"<script[\s\S]*?>[\s\S]*?<\/script>",text)):
+    if contains(scriptTag,scriptKeywords):
+      toBeRemoved.append(scriptTag)
+  return toBeRemoved
+
 def clean(filepath):
   with open(filepath, encoding="utf-8") as f:
     original=text=f.read()
-    
-#https://stackoverflow.com/questions/16585635/how-to-find-script-tag-from-the-string-with-javascript-regular-expression  
-    #https://stackoverflow.com/questions/500864/case-insensitive-regular-expression-without-re-compile
+
+  for literalsToRemove in findUnwantedScripts(text):
+    text = text.replace(literalsToRemove,"")     
   
   for tagToRemove in tagsToRemove:
     if verbose :
@@ -50,15 +75,21 @@ def clean(filepath):
         if len(i)>300:
           i=i[:150]+'\x1b[0;30;40m  ...  \x1b[0m'+i[-150:]
         print('\x1b[0;30;41m>>>\x1b[0m',i)
-    text = re.sub(tagToRemove,"",text,flags=re.IGNORECASE) 
+    #https://stackoverflow.com/questions/16585635/how-to-find-script-tag-from-the-string-with-javascript-regular-expression  
+    #https://stackoverflow.com/questions/500864/case-insensitive-regular-expression-without-re-compile
+    text = re.sub(tagToRemove,"",text,flags=re.IGNORECASE)
+  
   try:
     indexInsert=text.index('<html')+ len('<html')
-    text=text[:indexInsert]+' style="margin-top: 0px !important;	croll-padding-top: 0px !important;" '+text[indexInsert:]
+    text=text[:indexInsert]+' style="margin-top: 0px !important; scroll-padding-top: 0px !important;" '+text[indexInsert:]
   except:
-    print(f"error cannot find <html> tag in file @ {filepath}")
+    print(f"!Error: cannot find <html> tag in file @ {filepath}")
+
   text=text.replace("Blog at WordPress.com","Blog not at WordPress.com")
+  
   if original==text:
-    print(f"Warning: no change @ {filepath}")
+    print(f"!Warning: no change @ {filepath}")
+
   with open(filepath,'w',encoding="utf-8") as f:
     f.write(text)
 
@@ -78,12 +109,13 @@ for subdir, dirs, files in os.walk(resultPath):
       os.remove(path)
       print(path)
     else:
-      if clean(path):
-        print(path)
+      clean(path)
     if testOnlyOneFile:
       exit()
-print("Finished processing (at:",int(time.time()-startTime),"seconds)")
 
+print("Finished processing (at:",int(time.time()-startTime),"seconds)")
 if resultzip:
   os.system(f"zip -r result_{domain}.zip {resultPath}"+doNotShowShellResult)
   print("Finished zipping (at:",int(time.time()-startTime),"seconds)")
+
+print("DONE.")
