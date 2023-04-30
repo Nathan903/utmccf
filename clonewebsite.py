81335609d96bdcf3d418799adf2bec99809c2296
Nathan <nathanh4903@gmail.com>
2023-04-29 22:15:01 -0400

    added status.log+debug.log & timestamps

--- clonewebsite.py
+++ clonewebsite.py
@@ -4,13 +4,48 @@ downloadAgain=False
 resultzip=False
 verbose=False
 testOnlyOneFile=False
-removeExtraFiles=False
+removeExtraFiles=True
+
+import os, re,time, datetime, pytz
+startTime=time.time()
+resultPath=f'clean_{domain}'
+doNotShowShellResult=">run.log 2>&1"#">/dev/null 2>&1 "
+
+# functions for debugging and logging purposes. Not crucial
+def print_to_log_file(prefix, message=""):
+  with open(resultPath+"/debug.log", "a") as f:
+    f.write(prefix+" "+message+"\n")
+def print_and_log(message):
+  print_to_log_file(message)
+  print(message)
+def print_debug_info(message_str):
+  print_to_log_file("\t\tINFO:",message_str)
+def print_success(message_str):
+  print_to_log_file("\t\tSUCCESS:",message_str)
+warning_counter=0
+def print_warning(message_str):
+  global warning_counter
+  warning_counter+=1
+  print_to_log_file("WARNING:",message_str)
+  print("-"*10)
+  print("WARNING:",message_str)
+  print("-"*10)
+error_counter=0
+def print_error(message_str):
+  global error_counter
+  error_counter+=1
+  print("="*30)
+  print_to_log_file("ERROR:",message_str)
+  print("ERROR:",message_str)
+  print("="*30)
+
 
 import sys
 if len(sys.argv) > 1:
-  print(f"ARG: {sys.argv[1]}")
+  print_debug_info(f"ARG={sys.argv[1]}")
   if sys.argv[1]=="--downloadAgain":
     downloadAgain=True
+
   
 tagsToRemove=(
   #marketing bar
@@ -47,24 +82,17 @@ socialMediaIconSvg = {
   '#icon-instagram"></use>':'<path d="M12,4.622c2.403,0,2.688,0.009,3.637,0.052c0.877,0.04,1.354,0.187,1.671,0.31c0.42,0.163,0.72,0.358,1.035,0.673 c0.315,0.315,0.51,0.615,0.673,1.035c0.123,0.317,0.27,0.794,0.31,1.671c0.043,0.949,0.052,1.234,0.052,3.637 s-0.009,2.688-0.052,3.637c-0.04,0.877-0.187,1.354-0.31,1.671c-0.163,0.42-0.358,0.72-0.673,1.035 c-0.315,0.315-0.615,0.51-1.035,0.673c-0.317,0.123-0.794,0.27-1.671,0.31c-0.949,0.043-1.233,0.052-3.637,0.052 s-2.688-0.009-3.637-0.052c-0.877-0.04-1.354-0.187-1.671-0.31c-0.42-0.163-0.72-0.358-1.035-0.673 c-0.315-0.315-0.51-0.615-0.673-1.035c-0.123-0.317-0.27-0.794-0.31-1.671C4.631,14.688,4.622,14.403,4.622,12 s0.009-2.688,0.052-3.637c0.04-0.877,0.187-1.354,0.31-1.671c0.163-0.42,0.358-0.72,0.673-1.035 c0.315-0.315,0.615-0.51,1.035-0.673c0.317-0.123,0.794-0.27,1.671-0.31C9.312,4.631,9.597,4.622,12,4.622 M12,3 C9.556,3,9.249,3.01,8.289,3.054C7.331,3.098,6.677,3.25,6.105,3.472C5.513,3.702,5.011,4.01,4.511,4.511 c-0.5,0.5-0.808,1.002-1.038,1.594C3.25,6.677,3.098,7.331,3.054,8.289C3.01,9.249,3,9.556,3,12c0,2.444,0.01,2.751,0.054,3.711 c0.044,0.958,0.196,1.612,0.418,2.185c0.23,0.592,0.538,1.094,1.038,1.594c0.5,0.5,1.002,0.808,1.594,1.038 c0.572,0.222,1.227,0.375,2.185,0.418C9.249,20.99,9.556,21,12,21s2.751-0.01,3.711-0.054c0.958-0.044,1.612-0.196,2.185-0.418 c0.592-0.23,1.094-0.538,1.594-1.038c0.5-0.5,0.808-1.002,1.038-1.594c0.222-0.572,0.375-1.227,0.418-2.185 C20.99,14.751,21,14.444,21,12s-0.01-2.751-0.054-3.711c-0.044-0.958-0.196-1.612-0.418-2.185c-0.23-0.592-0.538-1.094-1.038-1.594 c-0.5-0.5-1.002-0.808-1.594-1.038c-0.572-0.222-1.227-0.375-2.185-0.418C14.751,3.01,14.444,3,12,3L12,3z M12,7.378 c-2.552,0-4.622,2.069-4.622,4.622S9.448,16.622,12,16.622s4.622-2.069,4.622-4.622S14.552,7.378,12,7.378z M12,15 c-1.657,0-3-1.343-3-3s1.343-3,3-3s3,1.343,3,3S13.657,15,12,15z M16.804,6.116c-0.596,0-1.08,0.484-1.08,1.08 s0.484,1.08,1.08,1.08c0.596,0,1.08-0.484,1.08-1.08S17.401,6.116,16.804,6.116z"></path>'
 }
 
-import os, re,time
-startTime=time.time()
-resultPath=f'clean_{domain}'
-doNotShowShellResult=">run.log 2>&1"#">/dev/null 2>&1 "
-
 def contains(text,keywordList):
   for keyword in keywordList:
     if keyword in text:
       return True
   return False
-
 def findUnwantedScripts(text): #look for all <script> tags
   toBeRemoved=[]
   for scriptTag in (re.findall(r"<script[\s\S]*?>[\s\S]*?<\/script>",text)):
     if contains(scriptTag,scriptKeywords):
       toBeRemoved.append(scriptTag)
   return toBeRemoved
-
 def clean(filepath):
   with open(filepath, encoding="utf-8") as f:
     original=text=f.read()
@@ -99,13 +127,12 @@ def clean(filepath):
     indexInsert=text.index('<html')+ len('<html')
     text=text[:indexInsert]+' style="margin-top: 0px !important; scroll-padding-top: 0px !important;" '+text[indexInsert:]
   except:
-    print(f"!Error: cannot find <html> tag in file @ {filepath}")
+    print_error(f"`clean()` - cannot find <html> tag in file in [{filepath}]")
 
   text=text.replace("Blog at WordPress.com","Blog not at WordPress.com")
   
   if original==text:
-    print(f"!Warning: no change @ {filepath}")
-
+    print_warning(f"`clean()` - no change in [{filepath}]")
   with open(filepath,'w',encoding="utf-8") as f:
     f.write(text)
 
@@ -113,25 +140,40 @@ def clean(filepath):
 if downloadAgain:
   downloadCommand=f'wget --reject xml,txt --reject-regex "(.*)\?(.*)|(.*)/feed/(.*)" --mirror --convert-links --adjust-extension --page-requisites --no-parent https://{domain}/'
   os.system(downloadCommand +doNotShowShellResult)
-  print("Finished download (at:",int(time.time()-startTime),"seconds)")
+  print_success(f"Finished download (at: {int(time.time()-startTime)} seconds)")
 
+#copy everything downloaded in /utmccf.wordpress.com into /clean_utmccf.wordpress.com
 from distutils.dir_util import copy_tree
 copy_tree(domain, resultPath)
 
+extensionToExcludeFromProduction = ('.py','.')
+keywordsToExcludeFromProduction = ('/?', '/feed/')
+extensionToNotClean = ('.txt','.xml', ".log")
+
+#main loop to clean all files in /clean_utmccf.wordpress.com
 for subdir, dirs, files in os.walk(resultPath):
   for file in files:
     path=os.path.join(subdir, file)
-    if removeExtraFiles and ('?' in path or 'feed' in path):
+    if removeExtraFiles and path.endswith(('.py','.')):
       os.remove(path)
-      print(path)
-    else:
-      clean(path)
-    if testOnlyOneFile:
-      exit()
+      print_debug_info(f"ignoring [{path}]. This is omitted in production website.")
+      continue 
 
-print("Finished processing (at:",int(time.time()-startTime),"seconds)")
+    if path.endswith(".html"):
+      clean(path)
+    elif path.endswith(extensionToNotClean):
+      #files to NOT clean
+      print_debug_info(f"downloaded file [{path}] is not an HTML. It is not processed for ad/tracker cleaning")
+    else:
+      print_warning(f"downloaded file [{path}] is not an HTML. It is not processed for ad/tracker cleaning")
+
+    #for debugging
+    if testOnlyOneFile: exit()
+
+print_success(f"Finished processing (at: {int(time.time()-startTime)} seconds)")
 if resultzip:
   os.system(f"zip -r result_{domain}.zip {resultPath}"+doNotShowShellResult)
-  print("Finished zipping (at:",int(time.time()-startTime),"seconds)")
+  print_success(f"Finished zipping (at: {int(time.time()-startTime)} seconds)")
+print_and_log(f"\t\tDONE: clonewebsite.py finished with {warning_counter} warnings and {error_counter} errors. Last updated {datetime.datetime.now(pytz.timezone('America/Toronto')).strftime('%Y-%m-%d %H:%M:%S %Z') }\n")
 
-print("DONE.")
+
