45a84bc5930fde9a732f408c1a3ee3745a9f401d
Nathan <nathanh4903@gmail.com>
2023-04-30 20:25:01 -0400

    check for broken absolute urls

--- clonewebsite.py
+++ clonewebsite.py
@@ -15,8 +15,8 @@ doNotShowShellResult=">run.log 2>&1"#">/dev/null 2>&1 "
 def print_to_log_file(prefix, message=""):
   with open(resultPath+"/debug.log", "a") as f:
     f.write(prefix+" "+message+"\n")
-def print_and_log(message):
-  print_to_log_file(message)
+def print_done(message):
+  print_to_log_file("\t\t"+message)
   print(message)
 def print_debug_info(message_str):
   print_to_log_file("\t\tINFO:",message_str)
@@ -27,18 +27,13 @@ def print_warning(message_str):
   global warning_counter
   warning_counter+=1
   print_to_log_file("WARNING:",message_str)
-  print("-"*10)
   print("WARNING:",message_str)
-  print("-"*10)
 error_counter=0
 def print_error(message_str):
   global error_counter
   error_counter+=1
-  print("="*30)
   print_to_log_file("ERROR:",message_str)
   print("ERROR:",message_str)
-  print("="*30)
-
 
 import sys
 if len(sys.argv) > 1:
@@ -81,6 +76,28 @@ socialMediaIconSvg = {
   '#icon-youtube"></use>':'<path d="M21.8,8.001c0,0-0.195-1.378-0.795-1.985c-0.76-0.797-1.613-0.801-2.004-0.847c-2.799-0.202-6.997-0.202-6.997-0.202 h-0.009c0,0-4.198,0-6.997,0.202C4.608,5.216,3.756,5.22,2.995,6.016C2.395,6.623,2.2,8.001,2.2,8.001S2,9.62,2,11.238v1.517 c0,1.618,0.2,3.237,0.2,3.237s0.195,1.378,0.795,1.985c0.761,0.797,1.76,0.771,2.205,0.855c1.6,0.153,6.8,0.201,6.8,0.201 s4.203-0.006,7.001-0.209c0.391-0.047,1.243-0.051,2.004-0.847c0.6-0.607,0.795-1.985,0.795-1.985s0.2-1.618,0.2-3.237v-1.517 C22,9.62,21.8,8.001,21.8,8.001z M9.935,14.594l-0.001-5.62l5.404,2.82L9.935,14.594z"></path>',
   '#icon-instagram"></use>':'<path d="M12,4.622c2.403,0,2.688,0.009,3.637,0.052c0.877,0.04,1.354,0.187,1.671,0.31c0.42,0.163,0.72,0.358,1.035,0.673 c0.315,0.315,0.51,0.615,0.673,1.035c0.123,0.317,0.27,0.794,0.31,1.671c0.043,0.949,0.052,1.234,0.052,3.637 s-0.009,2.688-0.052,3.637c-0.04,0.877-0.187,1.354-0.31,1.671c-0.163,0.42-0.358,0.72-0.673,1.035 c-0.315,0.315-0.615,0.51-1.035,0.673c-0.317,0.123-0.794,0.27-1.671,0.31c-0.949,0.043-1.233,0.052-3.637,0.052 s-2.688-0.009-3.637-0.052c-0.877-0.04-1.354-0.187-1.671-0.31c-0.42-0.163-0.72-0.358-1.035-0.673 c-0.315-0.315-0.51-0.615-0.673-1.035c-0.123-0.317-0.27-0.794-0.31-1.671C4.631,14.688,4.622,14.403,4.622,12 s0.009-2.688,0.052-3.637c0.04-0.877,0.187-1.354,0.31-1.671c0.163-0.42,0.358-0.72,0.673-1.035 c0.315-0.315,0.615-0.51,1.035-0.673c0.317-0.123,0.794-0.27,1.671-0.31C9.312,4.631,9.597,4.622,12,4.622 M12,3 C9.556,3,9.249,3.01,8.289,3.054C7.331,3.098,6.677,3.25,6.105,3.472C5.513,3.702,5.011,4.01,4.511,4.511 c-0.5,0.5-0.808,1.002-1.038,1.594C3.25,6.677,3.098,7.331,3.054,8.289C3.01,9.249,3,9.556,3,12c0,2.444,0.01,2.751,0.054,3.711 c0.044,0.958,0.196,1.612,0.418,2.185c0.23,0.592,0.538,1.094,1.038,1.594c0.5,0.5,1.002,0.808,1.594,1.038 c0.572,0.222,1.227,0.375,2.185,0.418C9.249,20.99,9.556,21,12,21s2.751-0.01,3.711-0.054c0.958-0.044,1.612-0.196,2.185-0.418 c0.592-0.23,1.094-0.538,1.594-1.038c0.5-0.5,0.808-1.002,1.038-1.594c0.222-0.572,0.375-1.227,0.418-2.185 C20.99,14.751,21,14.444,21,12s-0.01-2.751-0.054-3.711c-0.044-0.958-0.196-1.612-0.418-2.185c-0.23-0.592-0.538-1.094-1.038-1.594 c-0.5-0.5-1.002-0.808-1.594-1.038c-0.572-0.222-1.227-0.375-2.185-0.418C14.751,3.01,14.444,3,12,3L12,3z M12,7.378 c-2.552,0-4.622,2.069-4.622,4.622S9.448,16.622,12,16.622s4.622-2.069,4.622-4.622S14.552,7.378,12,7.378z M12,15 c-1.657,0-3-1.343-3-3s1.343-3,3-3s3,1.343,3,3S13.657,15,12,15z M16.804,6.116c-0.596,0-1.08,0.484-1.08,1.08 s0.484,1.08,1.08,1.08c0.596,0,1.08-0.484,1.08-1.08S17.401,6.116,16.804,6.116z"></path>'
 }
+warning_found_broken_absolute_url=False
+def check_for_broken_absolute_url(file_path, text):
+  global warning_found_broken_absolute_url
+  warning_found_broken_absolute_url=True
+  def absolute_url_to_relative(url):
+    keyword = "utmccf.com"
+    pos = url.find(keyword)
+    if pos != -1:
+      return url[pos+len(keyword):]
+    return url
+
+  #see https://stackoverflow.com/questions/904046/absolute-urls-relative-urls-and
+  # Define the pattern to search for
+  pattern = 'utmccf.com'
+  regexpattern = r"""     "[^@]{0,13}?utmccf\.com.{0,512}?"     """.strip()#.replace("$DOMAIN$",)
+  for line in text.split("\n"):
+    if re.search(regexpattern, line):
+      warning=f"This page 『 {file_path.replace('clean_','').replace('/index.html','')} 』 contains these absolute url links:\n"
+      for url in re.findall(regexpattern, line):
+        url=url.strip('"')
+        warning +=f"\t- 【 {url} 】\tfix it by changing it to:【 {absolute_url_to_relative(url)} 】\n"
+      print_warning(warning)
 
 def contains(text,keywordList):
   for keyword in keywordList:
@@ -133,6 +150,7 @@ def clean(filepath):
   
   if original==text:
     print_warning(f"`clean()` - no change in [{filepath}]")
+  check_for_broken_absolute_url(filepath,text)
   with open(filepath,'w',encoding="utf-8") as f:
     f.write(text)
 
@@ -174,6 +192,9 @@ print_success(f"Finished processing (at: {int(time.time()-startTime)} seconds)")
 if resultzip:
   os.system(f"zip -r result_{domain}.zip {resultPath}"+doNotShowShellResult)
   print_success(f"Finished zipping (at: {int(time.time()-startTime)} seconds)")
-print_and_log(f"\t\tDONE: clonewebsite.py finished with {warning_counter} warnings and {error_counter} errors. Last updated {datetime.datetime.now(pytz.timezone('America/Toronto')).strftime('%Y-%m-%d %H:%M:%S %Z') }\n")
+print_done(f"DONE: website published with {warning_counter} warnings and {error_counter} errors. Last updated {datetime.datetime.now(pytz.timezone('America/Toronto')).strftime('%Y-%m-%d %H:%M:%S %Z') }\n\n")
 
-
+if warning_counter!=0 or error_counter!=0:
+  print("* Website is published with warnings/errors. Please check all affected pages. If nothing is broken, ignore the warnings/errors")
+if warning_found_broken_absolute_url:
+  print("** Absolute url may or may not cause broken links on the website. Click on all links & buttons on the listed pages to see if they are broken")
